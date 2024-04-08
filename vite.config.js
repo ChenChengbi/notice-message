fc13636f6e182c6376983379fd4cdb9d3e37621a
Billy <1015018684@qq.com>
2024-04-08 17:47:09 +0800

    优化发布库和发布示例程序的打包配置

--- vite.config.js
+++ vite.config.js
@@ -7,31 +7,76 @@ import {
 import dts from 'vite-plugin-dts'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
-export default defineConfig({
-    build: {
-        lib: {
-            // Could also be a dictionary or array of multiple entry points
-            entry: resolve(__dirname, 'lib/main.js'),
-            name: 'Notice',
-            // the proper extensions will be added
-            // fileName: 'notice-message', // 默认 fileName 是 package.json 的 name 选项
-            formats: ['es', 'umd', 'iife'],
-        },
-        rollupOptions: {
-            // 确保外部化处理那些你不想打包进库的依赖
-            external: ['cash-dom'],
-            // output: {
-            //     // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-            //     globals: {
-            //         vue: 'Vue',
-            //     },
-            // },
-        },
-        copyPublicDir: false,
-    },
-    plugins: [
-        dts({ include: ['src/NoticeMessage'] }),
-        // Look at this: https://github.com/vitejs/vite/issues/1579
-        cssInjectedByJsPlugin(),
-    ],
+// export default defineConfig({
+//     // build: {
+//     //     lib: {
+//     //         // Could also be a dictionary or array of multiple entry points
+//     //         entry: resolve(__dirname, 'lib/main.js'),
+//     //         name: 'Notice',
+//     //         // the proper extensions will be added
+//     //         // fileName: 'notice-message', // 默认 fileName 是 package.json 的 name 选项
+//     //         formats: ['es', 'umd', 'iife'],
+//     //     },
+//     //     rollupOptions: {
+//     //         // 确保外部化处理那些你不想打包进库的依赖
+//     //         external: ['cash-dom'],
+//     //         // output: {
+//     //         //     // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+//     //         //     globals: {
+//     //         //         vue: 'Vue',
+//     //         //     },
+//     //         // },
+//     //     },
+//     //     copyPublicDir: false,
+//     // },
+//     // plugins: [
+//     //     dts({ include: ['src/NoticeMessage'] }),
+//     //     // Look at this: https://github.com/vitejs/vite/issues/1579
+//     //     cssInjectedByJsPlugin(),
+//     // ],
+//     base: './'
+// })
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    console.log('command, mode :>> ', command, mode);
+
+    if (mode === 'lib') {
+        return {
+            build: {
+                lib: {
+                    // Could also be a dictionary or array of multiple entry points
+                    entry: resolve(__dirname, 'lib/main.js'),
+                    name: 'Notice',
+                    // the proper extensions will be added
+                    // fileName: 'notice-message', // 默认 fileName 是 package.json 的 name 选项
+                    formats: ['es', 'umd', 'iife'],
+                },
+                rollupOptions: {
+                    // 确保外部化处理那些你不想打包进库的依赖
+                    external: ['cash-dom'],
+                    // output: {
+                    //     // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                    //     globals: {
+                    //         vue: 'Vue',
+                    //     },
+                    // },
+                },
+                copyPublicDir: false,
+            },
+            plugins: [
+                dts({ include: ['src/NoticeMessage'] }),
+                // Look at this: https://github.com/vitejs/vite/issues/1579
+                cssInjectedByJsPlugin(),
+            ],
+        }
+    } else if (mode === 'docs') {
+        return {
+            base: './',
+            build: {
+                outDir: "docs"
+            }
+        }
+    } else {
+        return {}
+    }
 })
